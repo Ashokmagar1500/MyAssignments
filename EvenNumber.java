78391f1520c1bee4b6fbd3683810ca1d244e2e09
Ashok Magar <110789544+Ashokmagar1500@users.noreply.github.com>
2023-02-07 18:14:18 -0500

    Add files via upload

---
+++ EvenNumber.java
@@ -0,0 +1,11 @@
+public class EvenNumber {
+    public static void main(String[] args) {
+        int n=30;
+        int i=3;
+        for (i=3; i<=n; i++)
+        {
+            if(i%2==0)
+            System.out.println(i + "  is even number.");
+        }
+    }
+}
---
+++ OddNumbers.java
@@ -0,0 +1,10 @@
+public class OddNumbers {
+    public static void main(String[] args) {
+        int n=20;
+        int i=2;
+        for(i=2; i<=n; i++){
+            if (i%2==1)
+                System.out.println(i +" is odd number");
+        }
+    }
+}
